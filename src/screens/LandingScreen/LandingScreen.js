802f83601de7ff8aa3f53f6ddac2917838240b57
Agniva Basak <agnivabasak1@gmail.com>
2020-11-22 02:09:24 +0530

    made the landing page responsive

--- src/screens/LandingScreen/LandingScreen.js
+++ src/screens/LandingScreen/LandingScreen.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React,{useState} from 'react';
 import './landingScreen.css';
 import ieee_vit_logo from '../../assets/ieee_vit_logo.png';
 import web_logo from '../../assets/web_logo.png';
@@ -7,14 +7,22 @@ import insta_logo from '../../assets/insta_logo.png';
 
 import SignupComponent from '../../uiComponents/SignupComponent/SignupComponent';
 const LandingScreen = () => {
-  return (
+    const [visibleComp,setVisibleComp] = useState(false);
+    function toggleVisibleComp() {
+        setVisibleComp(!visibleComp);
+    }
+    return (
       <div id = "screen-container">
         <div id="signup-component" >
-            <SignupComponent/>
+            <SignupComponent />
         </div>
+        <div id="signup-component-mobile" className={visibleComp?'visible-signup' : 'invisible-signup'}>
+            <SignupComponent closeComp={toggleVisibleComp}/>
+        </div>
+        <div id="overlay" onClick={toggleVisibleComp} className={visibleComp?'visible-overlay' : 'invisible-overlay'}></div>
         <div id="bg-1"></div>
         <div id="bg-2"></div>
-        <div>
+        <div style={{width : "100%"}}>
             <div id="ieee-logo">
                 <img src={ieee_vit_logo} />
             </div>
@@ -24,10 +32,11 @@ const LandingScreen = () => {
                 <h3>Do you have what it takes to be in this chapter of great minds...lol</h3>
             </div>
         </div>
+        <div id="register-btn" onClick={toggleVisibleComp}>Register Now</div>
         <div id="social-media-handles"> 
-            <img id="first-social-media-handle" src={web_logo} />
-            <img src={twitter_logo} />
-            <img src={insta_logo} />
+            <a href="https://ieeevit.org/"><img id="first-social-media-handle" src={web_logo} /></a>
+            <a href="https://twitter.com/ieeevitvellore" ><img src={twitter_logo} /></a>
+            <a href="https://www.instagram.com/ieeevitvellore/" ><img src={insta_logo} /></a>
         </div>
       </div>
   );
--- src/uiComponents/SignupComponent/SignupComponent.js
+++ src/uiComponents/SignupComponent/SignupComponent.js
@@ -5,14 +5,21 @@ import InputAdornment from '@material-ui/core/InputAdornment';
 import IconButton from '@material-ui/core/IconButton';
 import Visibility from '@material-ui/icons/Visibility';
 import VisibilityOff from '@material-ui/icons/VisibilityOff';
+import CloseIcon from '@material-ui/icons/Close';
 import Button from '@material-ui/core/Button';
 
-function SignupComponent() {
+function SignupComponent(props) {
     const [name,setName] = useState("") ;
     const [email,setEmail] = useState("") ;
     const [pass,setPass] = useState("") ;
     const [showPass, setShowPass] = useState(false);
     const [confirmPass,setConfirmPass] = useState("") ;
+    const [signUpIndicator,setSignUpIndicator] = useState(true);
+    const [logInIndicator,setLogInIndicator] = useState(false);
+    function toggleIndicators() {
+        setSignUpIndicator(!signUpIndicator);
+        setLogInIndicator(!logInIndicator);
+    }
     function toggleShowPass() {
        setShowPass(!showPass);
     }
@@ -20,15 +27,23 @@ function SignupComponent() {
         console.log("SUBMIT!");
     }
     return <div>
-        <div id="signup-container">
-            <div id="signup-header">
-                <h4>Signup</h4>
-                <h4>|</h4>
-                <h4>Login</h4>
+        <div id= {logInIndicator?"login-container":"signup-container"}>
+            <div id="close-icon" onClick={props.closeComp}>
+                <CloseIcon />
             </div>
-            <TextField className = "signup-elements" id="outlined-basic" type="text" value={name} label="Name" onChange={(newName)=>{setName(newName.target.value)}}/>
-            <TextField className = "signup-elements" id="outlined-basic" type="email" value={email} label="Email ID" onChange={(newEmail)=>{setEmail(newEmail.target.value)}}/>
-            <TextField className = "signup-elements" id="outlined-basic" type={showPass?'text':'password'} value={pass} label="Password" onChange={(newPass)=>{setPass(newPass.target.value)}} 
+            <div id="signup-header">
+                <h4 className={signUpIndicator?'thisison':'thisisoff'} onClick={()=>{if(!signUpIndicator) {toggleIndicators()}}}>Signup</h4>
+                <h4 style={{paddingBottom:"1vh"}}>|</h4>
+                <h4 className={logInIndicator?'thisison':'thisisoff'} onClick={()=>{if(!logInIndicator) {toggleIndicators()}}}>Login</h4>
+            </div>
+            <div className = {logInIndicator?"disappear" :"signup-elements"}>
+                <TextField style={{width:"100%"}} id="outlined-basic" type="text" value={name} label="Name" onChange={(newName)=>{setName(newName.target.value)}}/>
+            </div>
+            <div className = "signup-elements">
+               <TextField style={{width:"100%"}} id="outlined-basic" type="email" value={email} label="Email ID" onChange={(newEmail)=>{setEmail(newEmail.target.value)}}/>
+            </div>
+            <div className = "signup-elements">
+            <TextField style={{width:"100%"}}  id="outlined-basic" type={showPass?'text':'password'} value={pass} label="Password" onChange={(newPass)=>{setPass(newPass.target.value)}} 
                InputProps= {{
                 endAdornment : (
               <InputAdornment position="end">
@@ -42,7 +57,10 @@ function SignupComponent() {
               </InputAdornment> )
                }}
             />
-            <TextField className = "signup-elements" id="outlined-basic" type="password" value={confirmPass} label="Confirm Password" onChange={(newConfirmPass)=>{setConfirmPass(newConfirmPass.target.value)}}/>
+            </div>
+            <div className = {logInIndicator?"disappear" :"signup-elements"}>
+                 <TextField style={{width:"100%"}}  id="outlined-basic" type="password" value={confirmPass} label="Confirm Password" onChange={(newConfirmPass)=>{setConfirmPass(newConfirmPass.target.value)}}/>
+            </div>
             <div id="submit-button" onClick = {()=> handleSubmit()}>Submit</div>
         </div>
     </div> 
