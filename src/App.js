eba78c44c374551960651941bb21509ec7d53e9e
Agniva Basak <agnivabasak1@gmail.com>
2021-01-27 15:18:14 +0530

    Revert "thankyou screen"
    This reverts commit 0690e80bad14f974a7e22324fd23a1c11cbc8117.

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React from "react";
 
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 
-import Thankyou from "./screens/ThankyouScreen/ThankyouScreen";
 import LandingScreen from "./screens/LandingScreen/LandingScreen";
 import formScreen from "./screens/FormScreen/formScreen";
 import Dashboard from "./screens/Dashboard/dashboard";
@@ -18,7 +17,32 @@ const AppRouter = () => {
     <div>
       <BrowserRouter>
         <Switch>
-          <Route exact path="/" component={Thankyou} />
+          <ProtectedRoute
+            exact
+            path="/form"
+            component={formScreen}
+            redirect="/"
+          />
+          <ProtectedRoute
+            exact
+            path="/dashboard"
+            redirect="/"
+            component={Dashboard}
+          />
+          <Route
+            exact
+            path="/instructions"
+            redirect="/"
+            component={() => (
+              <div style={{ height: "100vh", background: "#0a0a0a" }}>
+                <Instructions />
+              </div>
+            )}
+          />
+          <Route exact path="/" component={LandingScreen} />
+          <Route exact path="/forgot-password" component={ForgotPassword} />
+          <Route exact path="/otp-verify" component={OtpVerif} />
+          <Route exact path="/reset-password" component={ResetPassword} />
           <Route exact path="" component={notFound} />
         </Switch>
       </BrowserRouter>
