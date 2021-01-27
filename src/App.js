f1fbb00c38dc1845eec4d2bf6b8f01916c8abb68
Agniva Basak <agnivabasak1@gmail.com>
2021-01-27 15:15:28 +0530

    Revert "merged results into main"
    This reverts commit 9c9c0bc8df1b80140525d06133d5c2c685cd9df2, reversing
    changes made to 0690e80bad14f974a7e22324fd23a1c11cbc8117.

--- src/App.js
+++ src/App.js
@@ -4,22 +4,21 @@ import { BrowserRouter, Route, Switch } from "react-router-dom";
 
 import Thankyou from "./screens/ThankyouScreen/ThankyouScreen";
 import LandingScreen from "./screens/LandingScreen/LandingScreen";
-// import formScreen from "./screens/FormScreen/formScreen";
-// import Dashboard from "./screens/Dashboard/dashboard";
-// import ForgotPassword from "./screens/ForgotPassword/ForgotPassword";
-// import ProtectedRoute from "./uiComponents/ProtectedRoute/ProtectedRoute";
-// import OtpVerif from "./screens/OtpVerif/OtpVerif";
-// import ResetPassword from "./screens/ResetPassword/ResetPassword";
+import formScreen from "./screens/FormScreen/formScreen";
+import Dashboard from "./screens/Dashboard/dashboard";
+import ForgotPassword from "./screens/ForgotPassword/ForgotPassword";
+import ProtectedRoute from "./uiComponents/ProtectedRoute/ProtectedRoute";
+import OtpVerif from "./screens/OtpVerif/OtpVerif";
+import ResetPassword from "./screens/ResetPassword/ResetPassword";
 import notFound from "./screens/NotFound/notFound";
-// import Instructions from "./uiComponents/Modal/Instructions";
-import ResultsHomeScreen from "./screens/ResultsHomeScreen/ResultsHomeScreen";
+import Instructions from "./uiComponents/Modal/Instructions";
 
 const AppRouter = () => {
   return (
     <div>
       <BrowserRouter>
         <Switch>
-          <Route exact path="/" component={ResultsHomeScreen} />
+          <Route exact path="/" component={Thankyou} />
           <Route exact path="" component={notFound} />
         </Switch>
       </BrowserRouter>
